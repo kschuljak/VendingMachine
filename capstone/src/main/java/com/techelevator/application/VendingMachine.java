a3f837a232adaf5c5b6ded5a717860f14392dec0
Katherine Schuljak <kschuljak@gmail.com>
2022-11-17 10:39:14 -0800

    added user output for testing flow

--- capstone/src/main/java/com/techelevator/application/VendingMachine.java
+++ capstone/src/main/java/com/techelevator/application/VendingMachine.java
@@ -1,5 +1,8 @@
 package com.techelevator.application;
 
+import com.techelevator.ui.UserInput;
+import com.techelevator.ui.UserOutput;
+
 public class VendingMachine
 {
     public void run()
@@ -7,7 +10,11 @@ public class VendingMachine
         while(true)
         {
             // display home screen
+            UserOutput.displayMainMenu();
+
+
             // get user selection
+            UserOutput.displayPurchaseMenu();
         }
     }    
 }
--- capstone/src/main/java/com/techelevator/ui/UserOutput.java
+++ capstone/src/main/java/com/techelevator/ui/UserOutput.java
@@ -1,4 +1,19 @@
 package com.techelevator.ui;
 
 public class UserOutput {
+
+    public static void displayMainMenu(){
+        System.out.println("1) Display Products");
+        System.out.println("2) Purchase");
+        System.out.println("3_ Exit");
+    }
+
+    public static void displayPurchaseMenu(){
+        System.out.println("Current Funds: ");
+        System.out.println();
+        System.out.println("1) Add Money");
+        System.out.println("2) Select Product;");
+        System.out.println("3) Finish Transaction");
+    }
+
 }
