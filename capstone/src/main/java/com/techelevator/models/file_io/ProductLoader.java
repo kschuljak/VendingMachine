af0f4b83f6ad8968ffcabbaf804dd724a4258978
Daniel Duque <dduque37@gmail.com>
2022-11-17 11:17:42 -0800

    Started ProductLoader & Product classes

--- capstone/src/main/java/com/techelevator/models/file_io/ProductLoader.java
+++ capstone/src/main/java/com/techelevator/models/file_io/ProductLoader.java
@@ -1,4 +1,25 @@
 package com.techelevator.models.file_io;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ProductLoader {
+
+    public static void LoadProductList()
+        {
+            File products = new File("data/vendingmachine.csv");
+            try (Scanner productloader = new Scanner(products))
+            {
+                while(productloader.hasNextLine())
+                {
+                    String line = productloader.nextLine();
+                    System.out.println(line);
+                }
+
+            }
+            catch (FileNotFoundException ex) {System.out.println(ex.getMessage());}
+        }
+
+
 }
--- capstone/src/main/java/com/techelevator/models/products/Product.java
+++ capstone/src/main/java/com/techelevator/models/products/Product.java
@@ -1,4 +1,49 @@
 package com.techelevator.models.products;
 
+import java.math.BigDecimal;
+
 public class Product {
+    private String slotID;
+    private String name;
+    private BigDecimal price;
+    private String type;
+
+    public String getSlotID()
+    {
+        return slotID;
+    }
+
+    // In case Umbrella Corp wants to re-organize the product display
+    public void setSlotID(String slotID) {this.slotID = slotID;}
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public BigDecimal getPrice()
+    {
+        return price;
+    }
+
+    // In case Umbrella Corp wants to update the prices later
+    public void setPrice(BigDecimal price) {this.price = price;}
+    
+
+    public Product()
+    {
+
+    }
+
+    public Product(String name, BigDecimal price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+
 }
